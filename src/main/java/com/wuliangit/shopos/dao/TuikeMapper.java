8b13fedd124147e7176aa7b2351c0a63f353a68b
taoshanchang@foxmail.com <chang123234>
2017-04-27 15:59:55 +0800

    tuike init

---
+++ src/main/java/com/wuliangit/shopos/dao/TuikeMapper.java
@@ -0,0 +1,7 @@
+package com.wuliangit.shopos.dao;
+
+import com.wuliangit.shopos.common.dao.BaseMapper;
+import com.wuliangit.shopos.entity.Tuike;
+
+public interface TuikeMapper extends BaseMapper<Tuike, Integer> {
+}
---
+++ src/main/java/com/wuliangit/shopos/entity/Tuike.java
@@ -0,0 +1,135 @@
+package com.wuliangit.shopos.entity;
+
+import java.util.Date;
+
+public class Tuike {
+    private Integer tuikeId;
+
+    private Integer memberId;
+
+    private String state;
+
+    private Boolean isCheck;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Long availableBalance;
+
+    private Long freezeBalance;
+
+    private String code;
+
+    private String shareCount;
+
+    private String buyCount;
+
+    private String alipayAccount;
+
+    private String bankCard;
+
+    public Integer getTuikeId() {
+        return tuikeId;
+    }
+
+    public void setTuikeId(Integer tuikeId) {
+        this.tuikeId = tuikeId;
+    }
+
+    public Integer getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public Boolean getIsCheck() {
+        return isCheck;
+    }
+
+    public void setIsCheck(Boolean isCheck) {
+        this.isCheck = isCheck;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Long getAvailableBalance() {
+        return availableBalance;
+    }
+
+    public void setAvailableBalance(Long availableBalance) {
+        this.availableBalance = availableBalance;
+    }
+
+    public Long getFreezeBalance() {
+        return freezeBalance;
+    }
+
+    public void setFreezeBalance(Long freezeBalance) {
+        this.freezeBalance = freezeBalance;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public String getShareCount() {
+        return shareCount;
+    }
+
+    public void setShareCount(String shareCount) {
+        this.shareCount = shareCount == null ? null : shareCount.trim();
+    }
+
+    public String getBuyCount() {
+        return buyCount;
+    }
+
+    public void setBuyCount(String buyCount) {
+        this.buyCount = buyCount == null ? null : buyCount.trim();
+    }
+
+    public String getAlipayAccount() {
+        return alipayAccount;
+    }
+
+    public void setAlipayAccount(String alipayAccount) {
+        this.alipayAccount = alipayAccount == null ? null : alipayAccount.trim();
+    }
+
+    public String getBankCard() {
+        return bankCard;
+    }
+
+    public void setBankCard(String bankCard) {
+        this.bankCard = bankCard == null ? null : bankCard.trim();
+    }
+}

84ca6bd5f7db8a4ca3f204f969f06c89ad26f1fd
taoshanchang@foxmail.com <chang123234>
2017-05-12 11:20:52 +0800

    权限相关

---
+++ src/main/java/com/wuliangit/shopos/dao/PermissionMapper.java
@@ -0,0 +1,10 @@
+package com.wuliangit.shopos.dao;
+
+import com.wuliangit.shopos.common.dao.BaseMapper;
+import com.wuliangit.shopos.entity.Permission;
+
+/**
+ * Created by nilme on 2017/5/12.
+ */
+public interface PermissionMapper extends BaseMapper<Permission, Integer> {
+}
---
+++ src/main/java/com/wuliangit/shopos/dao/RolePermissionMapper.java
@@ -0,0 +1,10 @@
+package com.wuliangit.shopos.dao;
+
+import com.wuliangit.shopos.common.dao.BaseMapper;
+import com.wuliangit.shopos.entity.RolePermission;
+
+/**
+ * Created by nilme on 2017/5/12.
+ */
+public interface RolePermissionMapper extends BaseMapper<RolePermission, Integer> {
+}

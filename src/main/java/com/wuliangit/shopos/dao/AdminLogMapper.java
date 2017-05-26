e387dcf3056f39ab17cd45d4fa81b486324d32ed
JangJanPing <810230010@qq.com>
2017-05-26 11:37:56 +0800

    admin log

--- src/main/java/com/wuliangit/shopos/dao/AdminLogMapper.java
+++ src/main/java/com/wuliangit/shopos/dao/AdminLogMapper.java
@@ -2,7 +2,17 @@ package com.wuliangit.shopos.dao;
 
 import com.wuliangit.shopos.common.dao.BaseMapper;
 import com.wuliangit.shopos.entity.AdminLog;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface AdminLogMapper extends BaseMapper<AdminLog, Integer> {
-
+    /**
+     * 查询管理员日志
+     * @param orderColumn
+     * @param orderType
+     * @param searchKey
+     * @return
+     */
+    List<AdminLog> selectAdminLogs(@Param("orderColumn") String orderColumn, @Param("orderType")String orderType, @Param("searchKey")String searchKey);
 }
--- src/main/java/com/wuliangit/shopos/service/impl/AdminLogServiceImpl.java
+++ src/main/java/com/wuliangit/shopos/service/impl/AdminLogServiceImpl.java
@@ -1,7 +1,10 @@
 package com.wuliangit.shopos.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.wuliangit.shopos.dao.AdminLogMapper;
 import com.wuliangit.shopos.entity.AdminLog;
 import com.wuliangit.shopos.service.AdminLogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +15,11 @@ import java.util.List;
 
 @Service
 public class AdminLogServiceImpl implements AdminLogService {
+    @Autowired
+    private AdminLogMapper adminLogMapper;
     @Override
     public List<AdminLog> getAdminLogList(Integer page, Integer pageSize, String orderColumn, String orderType, String searchKey) {
-        return null;
+        PageHelper.startPage(page, pageSize);
+        return adminLogMapper.selectAdminLogs(orderColumn, orderType, searchKey);
     }
 }

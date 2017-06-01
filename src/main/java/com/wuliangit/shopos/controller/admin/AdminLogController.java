1778781b337e0e20ee091d28a6770213d0fa4954
JangJanPing <810230010@qq.com>
2017-06-01 09:26:39 +0800

    admin log

--- src/main/java/com/wuliangit/shopos/controller/admin/AdminLogController.java
+++ src/main/java/com/wuliangit/shopos/controller/admin/AdminLogController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -32,6 +33,7 @@ public class AdminLogController {
     }
 
     @RequestMapping("/adminLogList")
+    @ResponseBody
     public Object searchAdminList(@RequestParam("draw") int draw,
                                   @RequestParam(value = "searchKey", required = false) String searchKey,
                                   @RequestParam(value = "orderColumn", required = false) String orderColumn,

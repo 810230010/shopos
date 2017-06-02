e41e89baa9e1c3954c68587f983519e3d93f3ff5
JangJanPing <810230010@qq.com>
2017-06-02 14:05:18 +0800

    upgrade

--- src/main/java/com/wuliangit/shopos/dto/StoreAnalyzeSimpl.java
+++ src/main/java/com/wuliangit/shopos/dto/StoreAnalyzeSimpl.java
@@ -1,100 +1,13 @@
 package com.wuliangit.shopos.dto;
 
+import org.apache.shiro.crypto.hash.Hash;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by nilme on 2017/6/1.
  */
-public class StoreAnalyzeSimpl {
-    //今日收入
-     private Map<String, Double> todayIncome;
-     //今日订单总数
-     private Map<String, Integer> todayOrders;
-     //今日评论数
-     private Map<String, Integer> todayComments;
-     //今日访问量
-     private Map<String, Integer> todayVisitors;
-     //未确认订单
-     private Map<String, Integer> unsuredOrders;
-     //待支付订单
-     private Map<String, Integer> waitTOPayOrders;
-    //待发货订单
-     private Map<String, Integer> waitToSendOrders;
-    //已成交
-    private Map<String, Integer> alreadyPayedOrders;
-    //退货申请
-    private Map<String, Integer> withdrawGoodsApplyNum;
+public class StoreAnalyzeSimpl extends HashMap<String, Object>{
 
-    public Map<String, Double> getTodayIncome() {
-        return todayIncome;
-    }
-
-    public void setTodayIncome(Map<String, Double> todayIncome) {
-        this.todayIncome = todayIncome;
-    }
-
-    public Map<String, Integer> getTodayOrders() {
-        return todayOrders;
-    }
-
-    public void setTodayOrders(Map<String, Integer> todayOrders) {
-        this.todayOrders = todayOrders;
-    }
-
-    public Map<String, Integer> getTodayComments() {
-        return todayComments;
-    }
-
-    public void setTodayComments(Map<String, Integer> todayComments) {
-        this.todayComments = todayComments;
-    }
-
-    public Map<String, Integer> getTodayVisitors() {
-        return todayVisitors;
-    }
-
-    public void setTodayVisitors(Map<String, Integer> todayVisitors) {
-        this.todayVisitors = todayVisitors;
-    }
-
-    public Map<String, Integer> getUnsuredOrders() {
-        return unsuredOrders;
-    }
-
-    public void setUnsuredOrders(Map<String, Integer> unsuredOrders) {
-        this.unsuredOrders = unsuredOrders;
-    }
-
-    public Map<String, Integer> getWaitTOPayOrders() {
-        return waitTOPayOrders;
-    }
-
-    public void setWaitTOPayOrders(Map<String, Integer> waitTOPayOrders) {
-        this.waitTOPayOrders = waitTOPayOrders;
-    }
-
-    public Map<String, Integer> getWaitToSendOrders() {
-        return waitToSendOrders;
-    }
-
-    public void setWaitToSendOrders(Map<String, Integer> waitToSendOrders) {
-        this.waitToSendOrders = waitToSendOrders;
-    }
-
-    public Map<String, Integer> getAlreadyPayedOrders() {
-        return alreadyPayedOrders;
-    }
-
-    public void setAlreadyPayedOrders(Map<String, Integer> alreadyPayedOrders) {
-        this.alreadyPayedOrders = alreadyPayedOrders;
-    }
-
-    public Map<String, Integer> getWithdrawGoodsApplyNum() {
-        return withdrawGoodsApplyNum;
-    }
-
-    public void setWithdrawGoodsApplyNum(Map<String, Integer> withdrawGoodsApplyNum) {
-        this.withdrawGoodsApplyNum = withdrawGoodsApplyNum;
-    }
 }
--- src/main/java/com/wuliangit/shopos/service/impl/AnalyzeServiceImpl.java
+++ src/main/java/com/wuliangit/shopos/service/impl/AnalyzeServiceImpl.java
@@ -22,49 +22,15 @@ public class AnalyzeServiceImpl implements AnalyzeService {
     @Override
     public StoreAnalyzeSimpl getStoreAnalysis(Integer storeId) {
         StoreAnalyzeSimpl analyze = new StoreAnalyzeSimpl();
-        //今日收入
-        Map<String, Double> todayIncome = new HashMap<String, Double>();
-        todayIncome.put("今日收入", analyzeMapper.getTodayIncome(storeId));
-
-        //今日订单总数
-        Map<String, Integer> todayOrders = new HashMap<String, Integer>();
-        todayOrders.put("今日订单数", analyzeMapper.getTodayOrders(storeId));
-
-        //今日评论数
-        Map<String, Integer> todayComments = new HashMap<String, Integer>();
-        todayComments.put("今日评论数", analyzeMapper.getTodayComments(storeId));
-
-        //今日访问量
-        Map<String, Integer> todayVisitors = new HashMap<String, Integer>();
-
-        //未确认订单
-        Map<String, Integer> unsuredOrders = new HashMap<String, Integer>();
-        unsuredOrders.put("未确认订单", analyzeMapper.getUnsuredOrders(storeId));
-
-        //待支付订单
-        Map<String, Integer> waitTOPayOrders = new HashMap<String, Integer>();
-        waitTOPayOrders.put("待支付订单", analyzeMapper.getWatiToPayOrders(storeId));
-
-        //待发货订单
-        Map<String, Integer> waitToSendOrders = new HashMap<String, Integer>();
-        waitToSendOrders.put("待发货", analyzeMapper.getWaitToSendGoods(storeId));
-
-        //已成交
-        Map<String, Integer> alreadyPayedOrders = new HashMap<String, Integer>();
-        alreadyPayedOrders.put("已成交", analyzeMapper.getDealFinishedOrders(storeId));
-
-        //退货申请
-        Map<String, Integer> withdrawGoodsApplyNum = new HashMap<String, Integer>();
-        withdrawGoodsApplyNum.put("退货申请", analyzeMapper.getWithdrawGoodsNum(storeId));
-
-        analyze.setTodayIncome(todayIncome);
-        analyze.setTodayOrders(todayOrders);
-        analyze.setTodayComments(todayComments);
-        analyze.setAlreadyPayedOrders(alreadyPayedOrders);
-        analyze.setUnsuredOrders(unsuredOrders);
-        analyze.setWaitTOPayOrders(waitTOPayOrders);
-        analyze.setWaitToSendOrders(waitToSendOrders);
-        analyze.setWithdrawGoodsApplyNum(withdrawGoodsApplyNum);
+          analyze.put("今日收入", analyzeMapper.getTodayIncome(storeId));
+          analyze.put("今日访问量", 200);
+          analyze.put("今日订单", analyzeMapper.getTodayOrders(storeId));
+          analyze.put("今日评论数", analyzeMapper.getTodayComments(storeId));
+          analyze.put("交易成功", analyzeMapper.getDealFinishedOrders(storeId));
+          analyze.put("未确认", analyzeMapper.getUnsuredOrders(storeId));
+          analyze.put("待付款", analyzeMapper.getWatiToPayOrders(storeId));
+          analyze.put("待发货", analyzeMapper.getWaitToSendGoods(storeId));
+          analyze.put("退货申请", analyzeMapper.getWithdrawGoodsNum(storeId));
         return analyze;
     }
 }

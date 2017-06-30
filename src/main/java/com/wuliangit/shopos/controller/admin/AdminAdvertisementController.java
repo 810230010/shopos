a45b5f8cf1d3343dc78fe7686ab2824358c76299
pwc <2622953398@qq.com>
2017-06-30 17:44:42 +0800

    todo

---
+++ src/main/java/com/wuliangit/shopos/controller/admin/AdminAdvertisementController.java
@@ -0,0 +1,15 @@
+package com.wuliangit.shopos.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wuliang01 on 2017/6/30.
+ */
+@Controller
+@RequestMapping("/admin/advertisement")
+public class AdminAdvertisementController {
+
+
+
+}
